ad53bd4efbd0b7049948dfca384af802f69b9a65
DESKTOP-28F38VP\user <nkc.yuki.hayashi@gmail.com>
2018-05-29 14:58:09 +0900

    外枠に入ったときにコンボの増加とスコアの増加、内枠に入ったらプラスの得点の追加、枠に入ってないときにライフ減少

---
+++ Assets/Script/GameMaster.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMaster : MonoBehaviour {
+
+	public static GameMaster gameMaster;
+
+	public Text countDownText;
+    public Text ComboText;
+    public Text ScoreText;
+   
+    public MouseCamera mouseCamera;
+
+	public event Action OnGameStart;
+	public event Action OnGameEnd;
+
+	bool isGameStart;
+    Slider _slider;
+
+
+    // Use this for initialization
+    void Awake () {
+		gameMaster = this;
+		countDownText.text = "";
+	}
+
+	void Start() {
+
+		StartCoroutine(CountDown());
+        _slider = GameObject.Find("Slider").GetComponent<Slider>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+       _slider.value =mouseCamera.life ;
+        ScoreText.text = mouseCamera.Score.ToString("");
+        ComboText.text = mouseCamera.Combo.ToString("");
+    }
+    
+	public void GameClear() {
+		//OnGameEnd?.Invoke();
+		isGameStart = false;
+	}
+
+	IEnumerator CountDown() {
+
+		yield return new WaitForSeconds(1);
+		countDownText.text = "3";
+		yield return new WaitForSeconds(1);
+		countDownText.text = "2";
+		yield return new WaitForSeconds(1);
+		countDownText.text = "1";
+		yield return new WaitForSeconds(1);
+		countDownText.text = "Queue";
+		yield return new WaitForSeconds(1);
+
+
+		countDownText.text = "";
+		if(OnGameStart != null)
+        {
+
+            OnGameStart();
+            //Debug.Log("a");
+        }
+        isGameStart = true;
+	}
+}
