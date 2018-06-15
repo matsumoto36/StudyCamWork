8914274f136175496bd1ebdb9ac1d1bc85a3ab8a
DESKTOP-28F38VP\user <nkc.daiki.nishiwaki@gmail.com>
2018-06-15 13:05:21 +0900

    クリア時のリザルト追加

--- Assets/Nishiwaki/GameEnd.cs
+++ Assets/Nishiwaki/GameEnd.cs
@@ -5,35 +5,107 @@ using UnityEngine.UI;
 
 public class GameEnd : MonoBehaviour
 {
+    //リザルトの背景
     public Image ResultBack;
+    //リザルトのタイトル
+    public Text Result;
+    //スコア　左
+    public Text ScoreFont;
+    //スコア　右
+    public Text ScoreNumber;
+    //コンボ　左
+    public Text ComboFont;
+    //コンボ　右
+    public Text ComboNumber;
+    //正確さ　左
+    public Text AccuracyFont;
+    //正確さ　右
+    public Text AccuracyNumber;
+
+    //リトライボタン
+    public Text Retry;
+    public Text ClearRetry;
+    //ステージセレクトボタン
+    public Text StageSelect;
+    public Text ClearStageSelect;
 
     // Use this for initialization
     void Start () {
-		//ResultBack = GetComponent<Image>();
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
-        //GameClear = ゲームクリアの判定
-        //if(GameClear == true && Input.GetMouseButtonDown(0))
-        if (Input.GetKeyDown(KeyCode.Z))
+        //ゲームクリア時
+        if (Input.GetKeyDown(KeyCode.Z))//判定を変更しておくこと
         {
-            StartCoroutine(Result());
+            StartCoroutine(GameClearResult());
+        }
+        //ゲームオーバー時
+        else if (Input.GetKeyDown(KeyCode.X))//判定を変更しておくこと
+        {
+            StartCoroutine(GameOverResult());
         }
     }
-    IEnumerator Result()
+
+    IEnumerator GameClearResult()
     {
         Color startColor = new Color(0, 0, 0, 0);
         Color endColor = new Color(0, 0, 0, 0.8f);
-        float time=0;
 
-        Debug.Log("コルーチン");
-        while (time<1.0f)
+        Color TextstartColor = new Color(1, 1, 1, 0);
+        Color TextendColor = new Color(1, 1, 1, 1);
+
+        ClearRetry.gameObject.SetActive(true);
+        ClearStageSelect.gameObject.SetActive(true);
+
+        //α値を変更する時間
+        float time = 0;
+
+        while (time < 1.0f)
         {
             time += Time.deltaTime;
             ResultBack.color = Color.Lerp(startColor, endColor, time);
+
+            Result.color = Color.Lerp(TextstartColor, TextendColor, time);
+            ScoreFont.color = Color.Lerp(TextstartColor, TextendColor, time);
+            ScoreNumber.color = Color.Lerp(TextstartColor, TextendColor, time);
+            ComboFont.color = Color.Lerp(TextstartColor, TextendColor, time);
+            ComboNumber.color = Color.Lerp(TextstartColor, TextendColor, time);
+            AccuracyFont.color = Color.Lerp(TextstartColor, TextendColor, time);
+            AccuracyNumber.color = Color.Lerp(TextstartColor, TextendColor, time);
+
+            ClearRetry.color = Color.Lerp(TextstartColor, TextendColor, time);
+            ClearStageSelect.color = Color.Lerp(TextstartColor, TextendColor, time);
+
             yield return null;
         }
         //yield return new WaitForSeconds(1);
+    }
+
+    IEnumerator GameOverResult()
+    {
+        Color startColor = new Color(0, 0, 0, 0);
+        Color endColor = new Color(0, 0, 0, 0.8f);
+
+        Color TextstartColor = new Color(1, 1, 1, 0);
+        Color TextendColor = new Color(1, 1, 1, 1);
+
+        Retry.gameObject.SetActive(true);
+        StageSelect.gameObject.SetActive(true);
+
+        //α値を変更する時間
+        float time = 0;
+
+        while (time < 1.0f)
+        {
+            time += Time.deltaTime * 3;
+            ResultBack.color = Color.Lerp(startColor, endColor, time);
+
+            Retry.color = Color.Lerp(TextstartColor, TextendColor, time);
+            StageSelect.color = Color.Lerp(TextstartColor, TextendColor, time);
+
+            yield return null;
+        }
     }
 }
---
+++ Assets/Nishiwaki/onclick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class onclick : MonoBehaviour {
+
+    public Button retry;
+
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            retry.onClick.Invoke();
+        }
+	}
+}
